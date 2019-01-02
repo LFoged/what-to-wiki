b5b12f8924e9a3cab4d2f7ca606c77cd5f3c7b52
Louis <louis.foged@gmail.com>
2019-01-02 17:57:37 +0100

    complete refactor of logic to use revealing module pattern & make it more maintainable

--- app.js
+++ app.js
@@ -1,69 +1,96 @@
 'use strict';
 
-/** DOM-RELATED ELEMENTS & FUNCTIONS **/
-const Dom = Object.freeze({
-  // DOM Elements
-  els: Object.freeze({
-    input: document.querySelector('.search-input'),
-    newSearchBtn: document.querySelector('.btn-new-search'),
-    searchSection: document.querySelector('.section__search'),
-    resultSection: document.querySelector('.section__results')
-  }),
-  // Method - create elements, assign classNames & attributes and values
-  makeEl: (rawElArray) => {
+
+// 'support' functions
+const UTILS = (() => {
+  const _doc = document;
+
+  // create elements, assign classNames & attributes and values
+  const makeEl = (rawElArray) => {
     return rawElArray.map((rawEl) => {
       const {el, cls, attr, val} = rawEl;
-      const newEl = document.createElement(el);
+      const newEl = _doc.createElement(el);
       newEl.className = cls;
       if (attr && val) attr.map((item, index) => newEl[item] = val[index]);
 
       return newEl;
     });  
-  },
-  // Method - append multiple child nodes to single parent element
-  appendKids: (parent, children) => {
+  };
+
+  // append multiple child nodes to single parent element
+  const appendKids = (parent, children) => {
     return children.map((child) => parent.appendChild(child));
-  },
-  // Method - remove all child nodes of a DOM element
-  removeKids: (element) => {
+  };
+
+  // remove all child nodes of a DOM element
+  const removeKids = (element) => {
     if (element.hasChildNodes()) {
       element.removeChild(element.firstChild);
 
-      return Dom.removeKids(element);
+      return UTILS.removeKids(element);
     }
-  },
-  // Method - display an alert message to DOM
-  errAlert: (msg='Aww shucks! Something went wrong') => {
-    const {makeEl, els} = Dom;
-    if (!document.querySelector('.alert-div')) {
+  };
+
+  return Object.freeze({ makeEl, appendKids, removeKids });
+})();
+
+
+// functions that directly deal with / 'touch' the DOM
+const DOM = ((UTILS) => {
+  const {makeEl, appendKids, removeKids} = UTILS;
+
+  const _doc = document;
+  const _templates = Object.freeze({
+    alert: (msg) => {
       const [alertDivEl, messageEl] = makeEl([
         {el: 'div', cls: 'alert-div'},
         {el: 'h3', cls: 'alert-msg', attr: ['textContent'], val: [msg]}
       ]);
       alertDivEl.appendChild(messageEl);
-      // Remove alertDiv from DOM after 2.7s
-      els.searchSection.insertBefore(alertDivEl, els.input);
-      setTimeout(() => {
-        document.querySelector('.alert-div').remove();
-      }, 2700);
-    }
-  },
-  // Method - print final results to DOM (both found articles & suggestions)
-  print: (data, els, makeEl, appendKids) => {
-    const {hits, queryText, articles, suggest} = data;
-    // results appended to document fragment later, so DOM only touched once
-    const fragment = document.createDocumentFragment();
-    // Function - create <p> displaying stats for returned articles
-    const prepStats = (hits, queryText, numArticles) => {
-      return makeEl([
+
+      return alertDivEl;
+    },
+    articleStats: (hits, queryText, numArticles) => {
+      const [stats] = makeEl([
         {
-          el: 'p', cls: 'stats', attr: ['textContent'],
+          el: 'p',
+          cls: 'stats',
+          attr: ['textContent'],
           val: [`Showing ${numArticles} of ${hits} hits for "${queryText}"`]
         }
       ]);
-    };
-    // Function - create <div> containing 'suggestion'
-    const prepSuggestion = (suggest, makeEl) => {
+
+      return stats; 
+    },
+    article: (article) => {
+      const articleLink = `https://en.wikipedia.org/wiki/${article.title.replace(/\s/, '_')}`;
+      const [resultDivEl, titleEl, snippetEl, wordCountEl, queryEl] = makeEl([
+        {el: 'div', cls: 'result__div'},
+        {
+          el: 'a',
+          cls: 'title article-link',
+          attr: ['href', 'target', 'textContent'],
+          val: [articleLink, '_blank', article.title]
+        },
+        {
+          el: 'p',
+          cls: 'snippet',
+          attr: ['innerHTML'],
+          val: [`${article.snippet}...`]},
+        {
+          el: 'p',
+          cls: 'word-count',
+          attr: ['textContent'],
+          val: [`Article Word Count: ${article.wordcount}`]
+        },
+        {el:'span', cls:'query-art', attr:['textContent'], val:[`Find More >>`]}
+      ]);  
+      wordCountEl.appendChild(queryEl);
+      appendKids(resultDivEl, [titleEl, snippetEl, wordCountEl]);
+
+      return resultDivEl;
+    },
+    suggestion: (suggest) => {
       const [resultDivEl, titleEl, queryEl] = makeEl([
         {el: 'div', cls: 'result__div'},
         {el: 'h3', cls: 'title', attr: ['textContent'], val: ['Suggested: ']},
@@ -73,105 +100,124 @@ const Dom = Object.freeze({
       resultDivEl.appendChild(titleEl);
 
       return resultDivEl;
-    };
-    // Function - create <div> containing data for received article
-    const prepArticle = (article) => (makeEl, appendKids) => {
-      const articleLink = `https://en.wikipedia.org/wiki/${article.title.replace(/\s/, '_')}`;
-      const [resultDivEl, titleEl, snippetEl, wordCountEl, queryEl] = makeEl([
-        {el: 'div', cls: 'result__div'},
-        {
-          el: 'a', cls: 'title article-link',
-          attr: ['href', 'target', 'textContent'],
-          val: [articleLink, '_blank', article.title]
-        },
-        {
-          el: 'p', cls: 'snippet', attr: ['innerHTML'],
-          val: [`${article.snippet}...`]},
-        {
-          el: 'p', cls: 'word-count', attr: ['textContent'],
-          val: [`Article Word Count: ${article.wordcount}`]
-        },
-        {el:'span', cls:'query-art', attr:['textContent'], val:[`Find More >>`]}
-      ]);  
-      wordCountEl.appendChild(queryEl);
-      appendKids(resultDivEl, [titleEl, snippetEl, wordCountEl]);
+    }
+  });
 
-      return resultDivEl;
-    };
-    // prep & print, either 'suggestion' or 'articles' & append to fragment
-    const [stats] = prepStats(hits, queryText, articles.length);
+  // DOM elements
+  const els = Object.freeze({
+    input: _doc.querySelector('.search-input'),
+    newSearchBtn: _doc.querySelector('.btn-new-search'),
+    searchSection: _doc.querySelector('.section__search'),
+    resultSection: _doc.querySelector('.section__results')
+  });
+
+  // print stats & articles or suggestions
+  const print = (data) => {
+    const {hits, queryText, articles, suggest} = data;
+
+    const fragment = _doc.createDocumentFragment();
+    // create relevant templates & append to fragment
+    const stats = _templates.articleStats(hits, queryText, articles.length);
     if (suggest) {
-      fragment.appendChild(prepSuggestion(suggest, makeEl));
+      fragment.appendChild(_templates.suggestion(suggest));
     } else {
-      // prep articles to be appended to fragment
-      const resultEls = articles.map(article => {
-        return prepArticle(article)(makeEl, appendKids);
+      const resultEls = articles.map(data => {
+        return _templates.article(data);
       });
       appendKids(fragment, resultEls);
     }
     fragment.insertBefore(stats, fragment.firstChild);
+    // display 'new search' btn & clear past results before appending new ones
     els.newSearchBtn.hidden = false;
+    if (els.resultSection.hasChildNodes()) removeKids(els.resultSection);
 
     return els.resultSection.appendChild(fragment);
-  },
+  };
 
-  // Method - clear results, hide 'newSearchBtn' & focus on input field
-  newSearch: (els, removeKids) => {
-    removeKids(els.resultSection);
-    els.newSearchBtn.hidden = true;
-    els.input.value = '';
-    els.input.focus();
-  }
-});
-
-/** DATA-RELATED FUNCTIONS **/
-const Data = Object.freeze({
-  // Method - check that input field isn't full of whitespace
-  checkInput: (input, errAlert) => {
-    if (input.length > 0 && (/^\s+$/).test(input)) {
-      return errAlert('Please enter a search query');
+  // display an alert message to DOM
+  const errAlert = (msg='Aww shucks! Something went wrong') => {
+    if (!_doc.querySelector('.alert-div')) {
+      const removeDelay = 2700;
+      const alert = _templates.alert(msg);
+      els.searchSection.insertBefore(alert, els.input);
+      // remove alert from DOM & remove after x secs
+      setTimeout(() => {
+        _doc.querySelector('.alert-div').remove();
+      }, removeDelay);
     }
+  };
 
-    return input.trim();
-  },
-  // Method - fetch (GET request) data from Wikipedia API for query text
-  makeRequest: (queryText, errAlert) => {
+  return Object.freeze({ els, print, errAlert });
+})(UTILS);
+
+
+// 'core' data-related functions
+const DATA = (() => {
+  // fetch (GET request) data from Wikipedia API
+  const makeRequest = (queryText) => {
     const url = `https://en.wikipedia.org/w/api.php?action=query&origin=*&list=search&format=json&srsearch=${queryText}`;
 
-    return fetch(url, {mode: 'cors'})
-      .then(response => response.json())
-      .catch(err => errAlert());
-  },
-  // Method - check response from request & extract relevant data 
-  filterResponse: (response) => {
+    return fetch(url, {mode: 'cors'}).then(response => response.json());
+  };
+
+  // check response from request & extract relevant data 
+  const filterResponse = (response) => {
     const hits = response.searchinfo.totalhits;
     const articles = response.search;
     const suggest = response.searchinfo.suggestion;
     if (articles.length < 1 && suggest) return {hits, articles, suggest};
 
     return {hits, articles};
-  }
-});
+  };
 
-// Function - initializes program => eventListeners & main control function 
-const init = (() => {
-  const {els, makeEl, appendKids, removeKids, errAlert, print, newSearch} = Dom;
-  const {checkInput, makeRequest, filterResponse} = Data;
-  // Function - main controller function
+  return Object.freeze({ makeRequest, filterResponse });
+})();
+
+
+// 'peripheral' (don't directly deal with data / DOM) functions
+const AUX = ((removeKids, els) => {
+  const {resultSection, newSearchBtn, input} = els;
+
+  // check input field isn't empty / just whitespace
+  const validateInput = (input) => {
+    return (input.length > 0 && (/^\s+$/).test(input)) ? false : input.trim();
+  };
+
+  // clear results, hide 'newSearchBtn' & put focus on input field
+  const newSearch = () => {
+    removeKids(resultSection);
+    newSearchBtn.hidden = true;
+    input.value = '';
+    input.focus();
+  };
+
+  return Object.freeze({ validateInput, newSearch });
+})(UTILS.removeKids, DOM.els);
+
+
+// initialize program => eventListeners & main control function 
+const init = ((DOM, DATA, AUX) => {
+  const {els, errAlert, print} = DOM;
+  const {makeRequest, filterResponse} = DATA;
+  const {validateInput, newSearch} = AUX;
+
+  // main controller function
   const ctrl = async (input) => {
-    const queryText = checkInput(input, errAlert);
-    if (queryText) {
-      const rawResponse = await makeRequest(queryText, errAlert);
+    const queryText = validateInput(input);
+    if (!queryText) return errAlert('Please enter a search query');
+    try {
+      const rawResponse = await makeRequest(queryText);
       const data = filterResponse(rawResponse.query);
       if (data.hits < 1) errAlert(`No results for "${queryText}"`);
-      if (els.resultSection.hasChildNodes()) removeKids(els.resultSection);
-      
-      return print({...data, queryText}, els, makeEl, appendKids);
+      print({...data, queryText});
+    } catch (error) {
+      errAlert();
     }
   };
+
   // Event-listeners (the magic starts here)
   els.input.addEventListener('keyup', (e) => ctrl(e.target.value));
-  els.newSearchBtn.addEventListener('click', () => newSearch(els, removeKids));
+  els.newSearchBtn.addEventListener('click', () => newSearch());
   els.resultSection.addEventListener('click', (e) => {
     if (e.target.className.includes('query')) {
       const currentInput = els.input.value.trim();
@@ -184,4 +230,4 @@ const init = (() => {
       ctrl(newInput);
     }
   });
-})();
+})(DOM, DATA, AUX);
